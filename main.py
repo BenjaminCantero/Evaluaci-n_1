4d006d11d4e4ebebd8ea0b82bfe2a124b97fdf50
BenjaminCantero <bcantero2023@alu.uct.cl>
2024-09-03 14:20:57 -0400

    limpieza
    se realiza una limpieza de rama para una restructuración de código y mejor ordenamiento de archivos

--- main.py
+++
@@ -1,8 +0,0 @@
-import customtkinter as ctk
-from restorant import App
-
-if __name__ == "__main__":
-    ctk.set_appearance_mode("dark")  
-    root = ctk.CTk()
-    app = App(root)
-    root.mainloop()
