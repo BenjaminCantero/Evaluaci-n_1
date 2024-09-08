6cab6d57758a19724bace2620bf7ed78244dca8e
imeduuu <128259631+imeduuu@users.noreply.github.com>
2024-09-08 11:25:03 -0300

    jijijija

--- gestor.py
+++ gestor.py
@@ -1,4 +1,5 @@
 from models import Ingrediente, Menu, Pedido
+from utils import generar_boleta
 
 class Controlador:
     def __init__(self):
@@ -7,18 +8,20 @@ class Controlador:
         self.pedido_actual = Pedido()
 
     def agregar_ingrediente(self, nombre, cantidad):
+        cantidad = int(cantidad)  # Asegúrate de convertir a entero después de validar
         for ingrediente in self.stock_ingredientes:
             if ingrediente.nombre == nombre:
                 ingrediente.cantidad += cantidad
                 return
+        
         nuevo_ingrediente = Ingrediente(nombre, cantidad)
         self.stock_ingredientes.append(nuevo_ingrediente)
 
 
     def eliminar_ingrediente(self, nombre):
-        # Lógica para eliminar ingredientes
-        pass
+        self.stock_ingredientes = [ing for ing in self.stock_ingredientes if ing.nombre != nombre]
+
 
     def generar_boleta(self):
-        # Lógica para generar la boleta
-        pass
+        generar_boleta(self.pedido_actual)
+
